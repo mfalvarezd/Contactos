1ad6e2058fa3d8c072295661f789ad601a43061d
Moises Alvarez <mfalvare@espol.edu.ec>
2023-11-03 15:06:07 -0500

    cambios en tdas

--- Contactos/src/main/java/com/espol/app/contactos/utilidades/List.java
+++ Contactos/src/main/java/com/espol/app/contactos/utilidades/List.java
@@ -7,7 +7,39 @@ package com.espol.app.contactos.utilidades;
 /**
  *
  * @author mfalvarez
+ * @param <E>
  */
-public class List {
+public interface List<E> extends Iterable<E> {
     
+    public int size();
+
+    public boolean isEmpty();
+
+    public void clear();
+
+    public boolean addFirst(E element); // inserta el elemento element al inicio
+
+    public boolean addLast(E element); // inserta el elemento element al final
+
+    public E removeFirst(); // remueve el elemento al inicio de la lista
+
+    public E removeLast(); // remueve el elemento al final de la lista
+
+    public boolean add(int index, E element); // inserta element en la posición index
+
+    public E remove(int index); // remueve y retorna el elemento en la posición index
+
+    public E get(int index); // retorna el elemento ubicado en la posición index
+
+    public E set(int index, E element); // setea el element en la posición index
+
+    @Override
+    public String toString();
+    
+    
+    public void reverse();
+    
+    public List<E> subList(int from, int to);
+    
+    public List<E> removeFirstNElements (int n);
 }
